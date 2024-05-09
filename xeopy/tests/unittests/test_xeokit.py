35c24e420387b02442936f8efbf568aab475ebce
paireks <w.radaczynski@gmail.com>
2024-05-09 21:21:07 +0200

    Remove test_xeokit.py

--- xeopy/tests/unittests/test_xeokit.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from xeopy import *
-
-
-def test_create_and_save():
-    xeokit = Xeokit(content=[Viewer(), CameraSettings(), IfcLoader(path="Duplex.ifc")], file_path="actual_created.html")
-    xeokit.create_and_save()
-
