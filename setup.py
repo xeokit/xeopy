8e0a537cc81b997c37b730e046762cbc86656f7e
paireks <w.radaczynski@gmail.com>
2024-05-24 16:12:32 +0200

    Try to add setup.py to check if it makes cloning easier

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name='xeopy',
+  packages=[],
+  version='0.0.0',
+  license='AGPL-3.0',
+  description='xeopy = xeokit + Python',
+  url='https://github.com/xeokit/xeopy',
+  keywords=[],
+  install_requires=[
+          'pytest',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10'
+  ],
+)
