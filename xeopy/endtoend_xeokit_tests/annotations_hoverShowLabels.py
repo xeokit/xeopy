d4292707b9cd1893cc7af03988970cab4dc85899
paireks <w.radaczynski@gmail.com>
2024-06-27 18:13:19 +0200

    Add annotations_hoverShowLabels.py

---
+++ xeopy/endtoend_xeokit_tests/annotations_hoverShowLabels.py
@@ -0,0 +1,27 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+def test_if_labels_are_visible_after_hover():
+    browser = webdriver.Firefox()
+    browser.get('https://xeokit.github.io/xeokit-sdk/examples/annotations/#annotations_hoverShowLabels')
+    time.sleep(2)
+    iframe = browser.find_element(By.ID, "viewer")
+    browser.switch_to.frame(iframe)
+    time.sleep(2)
+    annotation_markers = browser.find_elements(By.CLASS_NAME, "annotation-marker")
+    annotation_labels = browser.find_elements(By.CLASS_NAME, "annotation-label")
+
+    for i in range(3):
+        annotation_marker = annotation_markers[i]
+        annotation_label = annotation_labels[i]
+        assert annotation_label.is_displayed() is False
+        hover = ActionChains(browser).move_to_element(annotation_marker)
+        hover.perform()
+        time.sleep(0.5)
+        assert annotation_label.is_displayed() is True
+        time.sleep(1)
+
+    browser.quit()
